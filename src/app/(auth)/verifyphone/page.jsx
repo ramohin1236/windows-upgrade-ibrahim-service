abf4696f0ed44c6f0142896569ce84bd67a1bff0
ramohin1236 <mohinr26@gmail.com>
2025-09-02 13:27:02 +0600

    forget pass page create

---
+++ src/app/(auth)/verifyphone/page.jsx
@@ -0,0 +1,70 @@
+import registration_img from "../../../../public/login.svg";
+import React from "react";
+import Image from "next/image";
+
+const VerifyPhone = () => {
+  return (
+    <section className="">
+         <div className="max-w-[1100px] mx-auto h-[1200px] flex items-center justify-center max-h-screen  ">
+           <div className="flex items-center justify-center gap-8 bg-[#F8FAFC] rounded-sm overflow-clip shadow-2xl">
+             {/* Left Side - Images */}
+             <div className="hidden md:block overflow-hidden w-full h-full">
+               <div className="w-auto ">
+                 <Image
+                   src={registration_img}
+                   alt="Worker"
+                   className="w-full object-cover"
+                 />
+               </div>
+             </div>
+   
+             {/* Right Side - Role Selection */}
+             <div className="flex w-full items-center ">
+               <div>
+                 <div className=" flex flex-col items-center justify-center py-6 ">
+                   <div className="w-full">
+                     <div className="p-6 sm:p-8 ">
+                       <h1 className="text-[#394352] text-3xl font-semibold my-4">
+                        Verify Your Phone Number
+                       </h1>
+                       <p className="text-[#1F2937]">
+                         We'll send a verification code to this Phone Number to confirm your account.
+                       </p>
+                       {/* -------------------form------------------------------ */}
+                       <form className="mt-12 space-y-6">
+                         <div>
+                           <label className="text-[#1F2937] text-sm font-medium mb-2 block">
+                             Phone Number
+                           </label>
+                           <div className="relative flex items-center">
+                             <input
+                               name="phone_number"
+                               type="number"
+                               required
+                               className="w-full text-[#6B7280] text-sm border border-slate-300 px-4 py-3 pr-8 rounded-md outline-blue-600"
+                               placeholder="+123 445455455"
+                             />
+                           </div>
+                         </div>
+                   
+   
+                         <div className="mt-4 rounded-sm overflow-clip transition transform duration-300 hover:scale-101">
+                           <button className="bg-[#115E59] w-full py-2 text-white cursor-pointer ">
+                           Continue
+                           </button>
+                         </div>
+                       </form>
+                      
+                       
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </section>
+  )
+}
+
+export default VerifyPhone
